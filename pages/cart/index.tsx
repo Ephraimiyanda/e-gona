986284b176d234712d2ec8f065201a0ef6617c7e
Ephraimiyanda <iyandaephraim@gmail.com>
2023-10-05 06:47:09 +0100

    added sidebar and sellewr form

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -60,7 +60,6 @@ export default function Cart() {
               </div>
               <span>₦10,000</span>
             </div>
-            <Button className="text-white text-sm bg-[#A46E05BD] rounded-md py-2 px-4">Checkout (₦10,000.00)</Button>
           </div>
         </div>
       </div>
