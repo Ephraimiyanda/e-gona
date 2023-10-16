e93a7d828b5dccca0a28757a218d809a0b2815ea
Ephraimiyanda <iyandaephraim@gmail.com>
2023-10-16 03:31:45 +0100

    searchbar:styling finished

--- pages/savedItems/index.tsx
+++ pages/savedItems/index.tsx
@@ -7,7 +7,7 @@ export default function SavedItems(){
     const {savedItems,count} =useContext(AppContext)
     return (
         <div className="min-h-[50vh] m-auto">
-        <h1 className="px-6 py-2 text-xl font-semibold">SAVED ITEMS</h1>
+        <h1 className="px-6 py-2 text-xl font-semibold max-w-[1208px] m-auto">SAVED ITEMS</h1>
         <div className=" grid grid-cols-[repeat(auto-fill,minmax(220px,1fr))]  w-full gap-x-[1.50rem] gap-y-4 pt-10 max-w-[1280px] px-6 py-10 mx-auto ">
         {savedItems.length>0&&savedItems.map((items: { img: string; price: string; saleScale: string; title: string; }, index: number) => (
           <ProductCard
